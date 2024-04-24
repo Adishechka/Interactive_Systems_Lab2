c743bd09ad4b2022b47000e2222012946ed3f549
Adishechka <kamiladiev@yandex.ru>
2024-04-24 22:00:31 +0300

    k-adiev Lab2

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            comboBoxMode = new ComboBox();
+            buttonClick = new Button();
+            buttonStart = new Button();
+            buttonStop = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            textBox1 = new TextBox();
+            buttonClearTextBox = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // comboBoxMode
+            // 
+            comboBoxMode.Font = new Font("Cascadia Mono", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBoxMode.FormattingEnabled = true;
+            comboBoxMode.Items.AddRange(new object[] { "Режим 1", "Режим 2", "Режим 3" });
+            comboBoxMode.Location = new Point(933, 12);
+            comboBoxMode.Name = "comboBoxMode";
+            comboBoxMode.Size = new Size(121, 28);
+            comboBoxMode.TabIndex = 0;
+            // 
+            // buttonClick
+            // 
+            buttonClick.BackColor = Color.White;
+            buttonClick.Font = new Font("Arial", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonClick.Location = new Point(40, 180);
+            buttonClick.Margin = new Padding(5);
+            buttonClick.Name = "buttonClick";
+            buttonClick.Size = new Size(80, 80);
+            buttonClick.TabIndex = 1;
+            buttonClick.Text = "Кликни меня :3";
+            buttonClick.UseVisualStyleBackColor = false;
+            buttonClick.Click += buttonClick_Click;
+            // 
+            // buttonStart
+            // 
+            buttonStart.BackColor = Color.MediumSeaGreen;
+            buttonStart.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonStart.Location = new Point(1060, 12);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(101, 28);
+            buttonStart.TabIndex = 2;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = false;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonStop
+            // 
+            buttonStop.BackColor = Color.Orange;
+            buttonStop.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonStop.Location = new Point(1167, 12);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(101, 28);
+            buttonStop.TabIndex = 3;
+            buttonStop.Text = "Stop";
+            buttonStop.UseVisualStyleBackColor = false;
+            buttonStop.Click += buttonStop_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Info;
+            textBox1.CausesValidation = false;
+            textBox1.Font = new Font("Times New Roman", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(933, 80);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(440, 560);
+            textBox1.TabIndex = 5;
+            // 
+            // buttonClearTextBox
+            // 
+            buttonClearTextBox.BackColor = Color.LightSteelBlue;
+            buttonClearTextBox.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonClearTextBox.Location = new Point(1242, 46);
+            buttonClearTextBox.Name = "buttonClearTextBox";
+            buttonClearTextBox.Size = new Size(130, 28);
+            buttonClearTextBox.TabIndex = 6;
+            buttonClearTextBox.Text = "Clear TextBox";
+            buttonClearTextBox.UseVisualStyleBackColor = false;
+            buttonClearTextBox.Click += buttonClearTextBox_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(128, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Тестовая область:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(933, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(220, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Текстовая область (для чтения):";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(1384, 661);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(buttonClearTextBox);
+            Controls.Add(textBox1);
+            Controls.Add(buttonStop);
+            Controls.Add(buttonStart);
+            Controls.Add(buttonClick);
+            Controls.Add(comboBoxMode);
+            Font = new Font("Cascadia Mono", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Location = new Point(100, 100);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Lab2 - AdievKR";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBoxMode;
+        private Button buttonClick;
+        private Button buttonStart;
+        private Button buttonStop;
+        private System.Windows.Forms.Timer timer1;
+        private TextBox textBox1;
+        private Button buttonClearTextBox;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,340 @@
+﻿using System.Diagnostics;
+using System.Security.Policy;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lab2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Границы области тестирования
+        public int lineLeft = 20;
+        public int lineTop = 50;
+        public int lineRight = 900;
+        public int lineBottom = 400;
+
+        // Переменные для подстановки в формулу Фиттса
+        public int a = 1;
+        public double b = 0.5;
+        public double D = 1;
+        public double W = 1;
+
+
+        // Кол-во кликов
+        public const int countClicks = 10;
+        public int countClicksNow = 0;
+
+        // Массив хранения реального времени реакции User'а
+        public double[] arr = new double[countClicks];
+        public double[] arrFitts = new double[countClicks];
+
+        // Фиксация режима
+        public int comboBoxModeFixed = -1;
+
+        // Минимальное и максимально время для клика в мс
+        public int minRandomTimeSet = 1000;
+        public int maxRandomTimeSet = 3000;
+
+
+
+        public bool flagStopWatch;
+        private Stopwatch _stopWatch;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxMode.SelectedIndex = 0;
+            //Cursor.Position = new Point(Location.X + 400, Location.Y + 270);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g;
+
+            g = e.Graphics;
+
+            Pen myPenRect = new Pen(Color.Black, 2);
+
+            Rectangle rect = new Rectangle(lineLeft, lineTop, lineRight, lineBottom);
+
+            // Create solid brush.
+            SolidBrush blueBrush = new SolidBrush(Color.Gray);
+            g.FillRectangle(blueBrush, rect);
+            g.DrawRectangle(myPenRect, rect);
+
+
+            
+
+            //g.DrawLine(myPen, 1, 1, 45, 65);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Определение режима
+            switch(comboBoxMode.SelectedIndex)
+            {
+                case 0:
+                    comboBoxModeFixed = 0;
+                    break;
+                case 1:
+                    comboBoxModeFixed = 1;
+                    break;
+                case 2:
+                    comboBoxModeFixed = 2;
+                    break;
+            }
+
+            // Информирование о запущенном режиме
+            countClicksNow = 0;
+            buttonClick.Text = "Кликай";
+            textBox1.Text += "Режим " + (int)(comboBoxMode.SelectedIndex + 1) + " запущен...\r\n";
+
+            // Задаём случайный таймер от 1 до 3 секунд (в мс)
+            Random rnd = new Random();
+            timer1.Interval = rnd.Next(minRandomTimeSet, maxRandomTimeSet);
+            timer1.Enabled = true;
+        }
+
+        // Режим 1
+        public static void SetRandomPositionCursorMode0()
+        {
+            MessageBox.Show("Hello");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            if (comboBoxModeFixed == 0)
+            {
+                Cursor.Position = new Point(Location.X + buttonClick.Right + 200, Location.Y + buttonClick.Bottom + 30);
+
+                // Поскольку задаётся расстояние от кнопки вручную, то просто задаём фиксированное - 200
+                D = 200;
+                W = buttonClick.Right - buttonClick.Left; 
+
+            }
+            else if (comboBoxModeFixed == 1)
+            {
+                Random randHorizontal2 = new Random();
+
+                int randNext = randHorizontal2.Next(buttonClick.Right + 10, lineRight + 5 - Location.X);
+
+                Cursor.Position = new Point(Location.X + randNext, Location.Y + buttonClick.Bottom + 30);
+
+
+                // Расстояние от позиции курсора до нажимаемой кнопки
+                D = randNext - buttonClick.Right;
+                W = buttonClick.Right - buttonClick.Left;
+            }
+            else if (comboBoxModeFixed == 2)
+            {
+                Random buttonSizeWidth = new Random();
+                Random buttonSizeHeight = new Random();
+
+                buttonClick.Width = buttonSizeWidth.Next(40, 120);
+                buttonClick.Height = buttonSizeHeight.Next(40, 120);
+                //Random randHorizontal = new Random();
+                //Random randHorizontal = new Random();
+                //Cursor.Position = new Point(Location.X + randHorizontal2.Next(buttonClick.Right + 10, lineRight + 5 - Location.X), Location.Y + buttonClick.Bottom + 30);
+
+                Random randHorizontal = new Random();
+                Random randVertical = new Random();
+
+                int posX = randHorizontal.Next(lineLeft + 10, lineRight + 25);
+                int posY = randVertical.Next(lineTop + 30, lineBottom + 80);
+                while (
+                           (posX >= (buttonClick.Left + 10) && posX <= (buttonClick.Right + 10)) 
+                        || (posY >= (buttonClick.Top + 30) && posY <= (buttonClick.Bottom + 30))
+                      )
+                {
+                    //textBox1.Text += buttonClick.Left.ToString() + " "
+                    //               + buttonClick.Right.ToString() + " "
+                    //               + buttonClick.Top.ToString() + " "
+                    //               + buttonClick.Bottom.ToString() + "\r\n";
+                    posX = randHorizontal.Next(lineLeft + 10, lineRight + 25);
+                    posY = randVertical.Next(lineTop + 30, lineBottom + 80);
+                }
+
+                //Cursor.Position = new Point(Location.X + randHorizontal2.Next(buttonClick.Right + 10, lineRight + 5 - Location.X), Location.Y + buttonClick.Bottom + 30);
+                Cursor.Position = new Point(Location.X + posX, Location.Y + posY);
+
+            }
+
+
+
+            Random rnd = new Random();
+            _stopWatch = new Stopwatch();
+            flagStopWatch = true;
+            _stopWatch.Start();
+
+        }
+
+        private void buttonClick_Click(object sender, EventArgs e)
+        {
+            //Cursor.Position = new Point(Location.X + buttonClick.Right + 10, Location.Y + buttonClick.Top + 30);
+
+            //textBox1.Text += buttonClick.Left.ToString() + "\r\n";
+            //textBox1.Text += buttonClick.Right.ToString() + "\r\n";
+            //textBox1.Text += buttonClick.Top.ToString() + "\r\n";
+            //textBox1.Text += buttonClick.Bottom.ToString() + "\r\n";
+            //Cursor.Position = new Point(Location.X + buttonClick.Right + 10, Location.Y + buttonClick.Top + 30);
+
+            //Random randHorizontal2 = new Random();
+            //Cursor.Position = new Point(Location.X + randHorizontal2.Next(buttonClick.Right + 10, 905 - Location.X), Location.Y + buttonClick.Bottom + 30);
+            if (comboBoxModeFixed == -1)
+            {
+                textBox1.Text += "Никакой из режимов не запущен\r\n";
+            }
+            else
+            {
+                if (!flagStopWatch)
+                {
+                    textBox1.Text += "Дождись смещения курсора\r\n";
+                    return;
+                }
+                else 
+                {
+                    _stopWatch.Stop();
+                    flagStopWatch = false;
+                    double timeReaction = Math.Round(_stopWatch.Elapsed.TotalSeconds, 3);
+
+                    D /= 37.936267;
+                    W /= 37.936267;
+
+                    double timeReactionFitts = Math.Round(a + b * Math.Log2(D / W  + 1), 3);
+
+                    textBox1.Text += "Кнопка нажата за " + timeReaction.ToString() + " реальных секунд (" + (countClicksNow + 1).ToString() + ")\r\n";
+
+                    if (comboBoxModeFixed == 0 || comboBoxModeFixed == 1)
+                    {
+                        textBox1.Text += "Кнопка нажата за " + timeReactionFitts.ToString() + " секунд по формуле Фиттса (" + (countClicksNow + 1).ToString() + ")\r\n";
+                        D = 1;
+                        W = 1;
+                    }
+
+
+                    arr[countClicksNow] = timeReaction;
+                    arrFitts[countClicksNow] = timeReactionFitts;
+                    countClicksNow++;
+
+                    
+
+
+                    if (countClicksNow == countClicks) 
+                    {
+                        buttonClick.Text = "НЕ кликай";
+                        timer1.Enabled = false;
+
+                        double avgResult = 0;
+                        foreach (var item in arr)
+                        {
+                            avgResult += item;
+                        }
+                        avgResult = Math.Round(avgResult / countClicksNow, 3);
+
+                        double avgResultFitts = 0;
+                        foreach (var item in arrFitts)
+                        {
+                            avgResultFitts += item;
+                        }
+                        avgResultFitts = Math.Round(avgResultFitts / countClicksNow, 3);
+
+                        PrintStatistics(false, comboBoxModeFixed, countClicksNow, avgResult, avgResultFitts);
+                        comboBoxModeFixed = -1;
+                        countClicksNow = 0;
+                        System.Array.Clear(arr);
+                        System.Array.Clear(arrFitts);
+                        buttonClick.Width = 80;
+                        buttonClick.Height = 80;
+                    }
+                    else
+                    {
+                        // Задаём случайный таймер от 1 до 3 секунд(в мс) и продолжаем выполнение режима
+                        Random rnd = new Random();
+                        timer1.Interval = rnd.Next(minRandomTimeSet, maxRandomTimeSet);
+                        timer1.Enabled = true;
+                    }
+
+                }
+
+
+
+            }
+
+        }
+
+        private void buttonClearTextBox_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            buttonClick.Text = "НЕ кликай";
+            timer1.Enabled = false;
+            if (comboBoxModeFixed != -1)
+            {
+
+                double avgResult = 0;
+                foreach (var item in arr)
+                {
+                    avgResult += item;
+                }
+                avgResult = Math.Round(avgResult / countClicksNow, 3);
+
+                double avgResultFitts = 0;
+                foreach (var item in arrFitts)
+                {
+                    avgResultFitts += item;
+                }
+                avgResultFitts = Math.Round(avgResultFitts / countClicksNow, 3);
+
+                PrintStatistics(true, comboBoxModeFixed, countClicksNow, avgResult, avgResultFitts);
+            }
+            comboBoxModeFixed = -1;
+            countClicksNow = 0;
+            System.Array.Clear(arr);
+            System.Array.Clear(arrFitts);
+            buttonClick.Width = 80;
+            buttonClick.Height = 80;
+
+
+            //_stopWatch.Stop();
+        }
+
+        private void PrintStatistics(bool methodStop, int modeNumber, int totalTestCount, double avgResult, double avgResultFitts)
+        {
+
+
+            // Если остановлено вручную
+            if (methodStop)
+            {
+                textBox1.Text += "\r\nРежим " + (modeNumber + 1).ToString() + " завершён принудительно.";
+            }
+            else
+            {
+                textBox1.Text += "\r\nРежим " + (modeNumber + 1).ToString() + " подошёл к концу.";
+            }
+
+            // Доп. информация выводится только в случае нажатия хотя бы нескольких кнопок
+            if (countClicksNow > 0)
+            {
+                textBox1.Text += "\r\nВсего тестов: " + totalTestCount.ToString();
+                textBox1.Text += "\r\nСреднее реальное время нажатия кнопки: " + avgResult + " секунд.";
+
+                if (comboBoxModeFixed == 0 || comboBoxModeFixed == 1)
+                {
+                    textBox1.Text += "\r\nСреднее время нажатия кнопки по Фиттсу: " + avgResultFitts + " секунд.";
+                }
+            }
+            textBox1.Text += "\r\n";
+
+        }
+
+    }
+}
